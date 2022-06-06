eac9a54b1288aff7fb4935e7e1ec7a80601ce4ff
danielemalitesta <d.malitesta@gmail.com>
2022-06-06 10:35:01 +0200

    First commit.

--- external/models/__init__.py
+++ external/models/__init__.py
@@ -9,10 +9,12 @@ def import_model_by_backend(tensorflow_cmd, pytorch_cmd):
 
 import sys
 for _backend in sys.modules["external"].backend:
+    if _backend == "tensorflow":
+        pass
     elif _backend == "pytorch":
         from .ngcf.NGCF import NGCF
         from .lightgcn.LightGCN import LightGCN
         from .dgcf.DGCF import DGCF
-	from .ultragcn import UltraGCN
+        from .ultragcn import UltraGCN
         from .gfcf import GFCF
         from .lrgccf import LRGCCF
